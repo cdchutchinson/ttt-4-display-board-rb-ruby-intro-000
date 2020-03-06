ca6727dee75737c67535af95d3c0b16d4815ee08
Crystal <cdchutchinson@gmail.com>
2020-03-06 06:52:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board 
+def display_board
 puts "#{board = [" "," "," "," "," "," "," "," "," "]}"
-puts "#{[" ", " ", " ", " ", "X", " ", " ", " ", " "]}"    
+puts "#{[" ", " ", " ", " ", "X", " ", " ", " ", " "]}"
   end
