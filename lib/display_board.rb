9e3e8486405762fc3cb39dac99c28c837142d9f0
Crystal <cdchutchinson@gmail.com>
2020-03-06 07:05:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,18 +6,5 @@ def display_board
     puts "   |   |   "
   end
 
-  def display_board board=[" "," "," "," "," "," "," "," "," "]
-      puts "   |   |   "
-      puts "-----------"
-      puts "   |   |   "
-      puts "-----------"
-      puts "   |   |   "
-    end
 
-    def display_board board=[" ", " ", " ", " ", "X", " ", " ", " ", " "]
-        puts "   |   |   "
-        puts "-----------"
-        puts "   |   |   "
-        puts "-----------"
-        puts "   |   |   "
-      end
+
