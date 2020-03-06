50843686146f6fad2b03994b51288d2b7183534b
Crystal <cdchutchinson@gmail.com>
2020-03-06 08:18:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,9 +7,9 @@ def display_board
 end
 
 def display_board board= ["O"," "," "," "," "," "," "," "," "]
-  puts row[0]="{ O |   |   }"
-  puts row[1]="{-----------}"
-  puts row[2]="{   |   |   }"
-  puts row[3]="{-----------}"
-  puts row[4]="{   |   |   }"
+  puts "row[0]={ O |   |   }"
+  puts "row[1]={-----------}"
+  puts "row[2]={   |   |   }"
+  puts "row[3]={-----------}"
+  puts "row[4]={   |   |   }"
 end
